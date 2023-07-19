d0a534025783c8480b89ad3b7389c21fef0547dc
Yuzhou Liu <yuzhou.liu@mongodb.com>
2023-07-19 17:06:54 -0400

    remove flags for now

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -23,24 +23,9 @@ func NewRootCommand() *cobra.Command {
 		RunE:              root.runE,
 	}
 
-	cmd.PersistentFlags().String("log-level", "info", "Configure log level")
-	cmd.PersistentFlags().String("access-key", "", "AWS Access Key for ECR authentication")
-	cmd.PersistentFlags().String("secret-key", "", "AWS Secret Key for ECR authentication")
-	cmd.PersistentFlags().String("registry", "", "ECR registry")
-	cmd.PersistentFlags().String("repo", "", "The repository in the ECR registry")
-	cmd.PersistentFlags().String("command", "", "The buildah command to run")
-
-	addManifestCommandFlags(cmd)
-
 	cmd.AddCommand(newVersionCommand())
 
 	return cmd
-}
-
-func addManifestCommandFlags(cmd *cobra.Command) {
-	// flags specific to "manifest" command
-	cmd.PersistentFlags().StringSlice("sources", []string{}, "List of image tags to add to final manifest")
-	cmd.PersistentFlags().StringSlice("targets", []string{}, "List of tags to associate with final manifest")
 }
 
 func (c *RootCommand) persistentPreRunE(cmd *cobra.Command, args []string) error {
