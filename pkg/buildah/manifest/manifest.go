d836d92fff874e3d69d52b1036a91b4c33f5eda7
Yuzhou Liu <89029096+yuzhouliu9@users.noreply.github.com>
2023-07-31 14:33:48 -0400

    Add buildah manifest commands (#6)

--- pkg/buildah/manifest/manifest.go
+++ pkg/buildah/manifest/manifest.go
@@ -4,42 +4,55 @@ import (
 	"errors"
 	"fmt"
 	"os/exec"
+	"path"
+
+	"github.com/kanopy-platform/buildah-plugin/pkg/buildah/common"
 )
 
 const (
-	Command = "manifest"
+	manifestCommand        = "manifest"
+	storageDriverOptionVfs = "--storage-driver=vfs"
 )
 
 type (
 	CommandArgs struct {
-		Sources []string `json:"sources"`
-		Targets []string `json:"targets"`
+		Registry string
+		Repo     string
+		Sources  []string `json:"sources"`
+		Targets  []string `json:"targets"`
 	}
 )
 
 func (c *CommandArgs) GetCmds() ([]*exec.Cmd, error) {
-	cmds := []*exec.Cmd{}
-
 	addCmds, err := c.isManifestCmd()
 	if err != nil {
-		return cmds, err
+		return nil, err
 	}
 
 	if !addCmds {
-		return cmds, nil
+		return nil, nil
 	}
 
-	// TODO add commands to run.
-
-	return cmds, nil
+	return c.manifestCommands(), nil
 }
 
 func (c *CommandArgs) isManifestCmd() (bool, error) {
-	if len(c.Sources) == 0 && len(c.Targets) == 0 {
+	if c.Registry == "" &&
+		c.Repo == "" &&
+		len(c.Sources) == 0 &&
+		len(c.Targets) == 0 {
 		return false, nil
 	}
 
 	var err error
+
+	if c.Registry == "" {
+		err = errors.Join(err, fmt.Errorf("manifest command: registry must be specified"))
+	}
+
+	if c.Repo == "" {
+		err = errors.Join(err, fmt.Errorf("manifest command: repo must be specified"))
+	}
 
 	if len(c.Sources) == 0 {
 		err = errors.Join(err, fmt.Errorf("manifest command: sources must be specified"))
@@ -50,4 +63,25 @@ func (c *CommandArgs) isManifestCmd() (bool, error) {
 	}
 
 	return true, err
+}
+
+func (c *CommandArgs) manifestCommands() []*exec.Cmd {
+	cmds := []*exec.Cmd{}
+
+	registryRepo := path.Join(c.Registry, c.Repo)
+
+	for _, targetTag := range c.Targets {
+		target := fmt.Sprintf("%s:%s", registryRepo, targetTag)
+
+		cmds = append(cmds, exec.Command(common.BuildahCmd, manifestCommand, "create", storageDriverOptionVfs, target))
+
+		for _, sourceTag := range c.Sources {
+			source := fmt.Sprintf("%s:%s", registryRepo, sourceTag)
+			cmds = append(cmds, exec.Command(common.BuildahCmd, manifestCommand, "add", storageDriverOptionVfs, target, source))
+		}
+
+		cmds = append(cmds, exec.Command(common.BuildahCmd, manifestCommand, "push", storageDriverOptionVfs, "--all", target))
+	}
+
+	return cmds
 }
--- pkg/buildah/manifest/manifest_test.go
+++ pkg/buildah/manifest/manifest_test.go
@@ -1,8 +1,10 @@
 package manifest
 
 import (
+	"os/exec"
 	"testing"
 
+	"github.com/kanopy-platform/buildah-plugin/pkg/buildah/common"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -21,22 +23,28 @@ func TestIsManifestCmd(t *testing.T) {
 		},
 		"all required args exist": {
 			args: CommandArgs{
-				Sources: []string{"a"},
-				Targets: []string{"b"},
+				Registry: "a",
+				Repo:     "b",
+				Sources:  []string{"a"},
+				Targets:  []string{"b"},
 			},
 			wantResult: true,
 			wantErr:    false,
 		},
 		"missing sources": {
 			args: CommandArgs{
-				Targets: []string{"b"},
+				Registry: "a",
+				Repo:     "b",
+				Targets:  []string{"b"},
 			},
 			wantResult: true,
 			wantErr:    true,
 		},
 		"missing targets": {
 			args: CommandArgs{
-				Sources: []string{"a"},
+				Registry: "a",
+				Repo:     "b",
+				Sources:  []string{"a"},
 			},
 			wantResult: true,
 			wantErr:    true,
@@ -49,5 +57,42 @@ func TestIsManifestCmd(t *testing.T) {
 		result, err := test.args.isManifestCmd()
 		assert.Equal(t, test.wantResult, result)
 		assert.Equal(t, test.wantErr, err != nil)
+	}
+}
+
+func TestManifestCommands(t *testing.T) {
+	t.Parallel()
+
+	tests := map[string]struct {
+		commandArgs CommandArgs
+		wantCmds    []*exec.Cmd
+	}{
+		"multiple sources, multiple targets": {
+			commandArgs: CommandArgs{
+				Registry: "public.ecr.aws",
+				Repo:     "devops",
+				Sources:  []string{"a", "b"},
+				Targets:  []string{"1", "2"},
+			},
+			wantCmds: []*exec.Cmd{
+				// target 1
+				exec.Command(common.BuildahCmd, manifestCommand, "create", storageDriverOptionVfs, "public.ecr.aws/devops:1"),
+				exec.Command(common.BuildahCmd, manifestCommand, "add", storageDriverOptionVfs, "public.ecr.aws/devops:1", "public.ecr.aws/devops:a"),
+				exec.Command(common.BuildahCmd, manifestCommand, "add", storageDriverOptionVfs, "public.ecr.aws/devops:1", "public.ecr.aws/devops:b"),
+				exec.Command(common.BuildahCmd, manifestCommand, "push", storageDriverOptionVfs, "--all", "public.ecr.aws/devops:1"),
+				// target 2
+				exec.Command(common.BuildahCmd, manifestCommand, "create", storageDriverOptionVfs, "public.ecr.aws/devops:2"),
+				exec.Command(common.BuildahCmd, manifestCommand, "add", storageDriverOptionVfs, "public.ecr.aws/devops:2", "public.ecr.aws/devops:a"),
+				exec.Command(common.BuildahCmd, manifestCommand, "add", storageDriverOptionVfs, "public.ecr.aws/devops:2", "public.ecr.aws/devops:b"),
+				exec.Command(common.BuildahCmd, manifestCommand, "push", storageDriverOptionVfs, "--all", "public.ecr.aws/devops:2"),
+			},
+		},
+	}
+
+	for name, test := range tests {
+		t.Log(name)
+
+		result := test.commandArgs.manifestCommands()
+		assert.Equal(t, test.wantCmds, result)
 	}
 }
